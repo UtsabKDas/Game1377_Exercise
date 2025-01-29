d21cc44643307efc91240f956187d889964df54c
UtsabKDas <utsabkdas@gmail.com>
2025-01-28 19:54:08 -0500

    Add project files.

---
+++ Game1377_Exercise/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Game1377_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
